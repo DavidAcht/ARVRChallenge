43081bbb00bccc426cc4801871b5b362be519028
DavidAcht <45337301+DavidAcht@users.noreply.github.com>
2020-05-22 16:34:19 +0200

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+document.onclick = function(){
+var audio = new Audio('../assets/music.mp3');
+audio.play();
+}
+// var camera = document.getElementById("camera");
+// var direction = new THREE.Vector3().copy(camera.getWorldDirection());
+
+// function draw(){
+// tick : function move()
+// {
+//     var rig = document.getElementById("rig");
+//     var stepFactor = 0.1;
+//     rig.parent.position.add(this.direction.multiplyScalar(stepFactor));
+// }
+// }
+
+
