7f7a3d9201636c3c4806da872170727ec0575153
DavidAcht <45337301+DavidAcht@users.noreply.github.com>
2020-05-22 18:06:24 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
 document.onclick = function(){
+	document.getElementById("timerH1").style.display = "block";
+	document.getElementById("waitH1").style.display = "none";
 var audio = new Audio('assets/music.mp3');
 audio.play();
+var fiveMinutes = 60 * 2.63,
+        display = document.querySelector('#timer');
+    startTimer(fiveMinutes, display);
+
+
+
+
 }
 // var camera = document.getElementById("camera");
 // var direction = new THREE.Vector3().copy(camera.getWorldDirection());
